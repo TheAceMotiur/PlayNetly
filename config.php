d19be7af473e3e75ce395604ea63e7cc95b7c5c6
OneNetly <admin@onenetly.com>
2024-09-29 01:55:02 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$db_user = "root";
-$db_password = "";
-$db_name = "oneweb";
+$host = "207.244.240.126";
+$db_user = "playnetl_y";
+$db_password = "AmiMotiur27@";
+$db_name = "playnetl_y";
 
 // Create connection
 $conn = new mysqli($host, $db_user, $db_password, $db_name);
